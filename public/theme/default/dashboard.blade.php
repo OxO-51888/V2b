61918644673483a23f000b31577dff8544d84e19
OxO-51888 <OxO-51888@users.noreply.github.com>
2026-06-19 22:36:35 +0800

    Add subscription rule security controls

--- public/theme/default/dashboard.blade.php
+++ public/theme/default/dashboard.blade.php
@@ -1,21 +1,63 @@
-<!DOCTYPE html>
+﻿<!DOCTYPE html>
 <html>
 
 <head>
     <link rel="stylesheet" href="/theme/{{$theme}}/assets/components.chunk.css?v={{$version}}">
     <link rel="stylesheet" href="/theme/{{$theme}}/assets/umi.css?v={{$version}}">
+    @php ($themeColor = $theme_config['theme_color'] ?? 'default')
+    @php ($themeCssPath = public_path("theme/{$theme}/assets/theme/{$themeColor}.css"))
+    @php ($themeOverrideCssPath = public_path("theme/{$theme}/assets/theme/{$themeColor}-overrides.css"))
+    @php ($preloadThemeColors = array_unique(['default', 'darkblue']))
+    @foreach ($preloadThemeColors as $preloadThemeColor)
+        @continue ($preloadThemeColor === $themeColor)
+        @php ($preloadThemeCssPath = public_path("theme/{$theme}/assets/theme/{$preloadThemeColor}.css"))
+        @php ($preloadThemeOverrideCssPath = public_path("theme/{$theme}/assets/theme/{$preloadThemeColor}-overrides.css"))
+        @if (file_exists($preloadThemeCssPath))
+            <link rel="preload" as="style" href="/theme/{{$theme}}/assets/theme/{{$preloadThemeColor}}.css?v={{$version}}-{{filemtime($preloadThemeCssPath)}}">
+        @endif
+        @if (file_exists($preloadThemeOverrideCssPath))
+            <link rel="preload" as="style" href="/theme/{{$theme}}/assets/theme/{{$preloadThemeColor}}-overrides.css?v={{$version}}-{{filemtime($preloadThemeOverrideCssPath)}}">
+        @endif
+    @endforeach
+    @if (file_exists($themeCssPath))
+        <link rel="stylesheet" href="/theme/{{$theme}}/assets/theme/{{$themeColor}}.css?v={{$version}}-{{filemtime($themeCssPath)}}">
+    @endif
+    @if (file_exists($themeOverrideCssPath))
+        <link rel="stylesheet" href="/theme/{{$theme}}/assets/theme/{{$themeColor}}-overrides.css?v={{$version}}-{{filemtime($themeOverrideCssPath)}}">
+    @endif
     @if (file_exists(public_path("/theme/{$theme}/assets/custom.css")))
-        <link rel="stylesheet" href="/theme/{{$theme}}/assets/custom.css?v={{$version}}">
+        <link rel="stylesheet" href="/theme/{{$theme}}/assets/custom.css?v={{$version}}-{{filemtime(public_path("theme/{$theme}/assets/custom.css"))}}">
     @endif
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,minimum-scale=1,user-scalable=no">
     @php ($colors = [
-        'darkblue' => '#3b5998',
+        'darkblue' => '#e184ad',
         'black' => '#343a40',
         'default' => '#0665d0',
         'green' => '#319795'
     ])
-    <meta name="theme-color" content="{{$colors[$theme_config['theme_color']]}}">
+    <meta name="theme-color" content="{{$colors[$themeColor] ?? $colors['default']}}">
+    {{-- dashboard_inline_pink_header_unify --}}
+    @if ($themeColor === 'darkblue')
+        <style>
+            html body #page-container #page-header,
+            html body #page-container #page-header > .content-header,
+            html body #page-container #sidebar > .smini-hidden,
+            html body #page-container #sidebar > .smini-hidden.bg-header-dark,
+            html body #page-container #sidebar > .smini-hidden > .content-header,
+            html body #page-container #sidebar > .smini-hidden > .content-header.bg-white-10,
+            html body #page-container #sidebar > .smini-hidden > .content-header > a,
+            html body #page-container #sidebar > .smini-hidden > .content-header > a > span {
+                background: #e184ad !important;
+                background-color: #e184ad !important;
+                background-image: none !important;
+                box-shadow: none !important;
+                opacity: 1 !important;
+                filter: none !important;
+                mix-blend-mode: normal !important;
+            }
+        </style>
+    @endif
 
     <title>{{$title}}</title>
     <!-- <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,400,400i,600,700"> -->
@@ -27,7 +69,7 @@
             theme: {
                 sidebar: '{{$theme_config['theme_sidebar']}}',
                 header: '{{$theme_config['theme_header']}}',
-                color: '{{$theme_config['theme_color']}}',
+                color: '{{$themeColor}}',
             },
             version: '{{$version}}',
             background_url: '{{$theme_config['background_url']}}',
@@ -58,10 +100,9 @@
 {!! $theme_config['custom_html'] !!}
 <script src="/theme/{{$theme}}/assets/vendors.async.js?v={{$version}}"></script>
 <script src="/theme/{{$theme}}/assets/components.async.js?v={{$version}}"></script>
-<script src="/theme/{{$theme}}/assets/umi.js?v={{$version}}"></script>
+<script src="/theme/{{$theme}}/assets/umi.js?v={{$version}}-{{filemtime(public_path('theme/'.$theme.'/assets/umi.js'))}}"></script>
 @if (file_exists(public_path("/theme/{$theme}/assets/custom.js")))
     <script src="/theme/{{$theme}}/assets/custom.js?v={{$version}}"></script>
-@endif
-</body>
+@endif</body>
 
 </html>
