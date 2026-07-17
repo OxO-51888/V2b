1d4e917d18543ba79733a4979f7541a013f08b00
OxO-51888 <OxO-51888@users.noreply.github.com>
2026-07-17 20:04:41 +0800

    Hide Telegram group without active plan

--- app/Http/Controllers/V1/User/CommController.php
+++ app/Http/Controllers/V1/User/CommController.php
@@ -4,17 +4,25 @@ namespace App\Http\Controllers\V1\User;
 
 use App\Http\Controllers\Controller;
 use App\Models\Payment;
+use App\Models\User;
 use App\Utils\Dict;
 use Illuminate\Http\Request;
 
 class CommController extends Controller
 {
-    public function config()
+    public function config(Request $request)
     {
+        $user = User::select(['plan_id', 'expired_at'])->find($request->user['id']);
+        $hasActivePlan = $user
+            && (int)$user->plan_id > 0
+            && ($user->expired_at === null || (int)$user->expired_at > time());
+
         return response([
             'data' => [
                 'is_telegram' => (int)config('v2board.telegram_bot_enable', 0),
-                'telegram_discuss_link' => config('v2board.telegram_discuss_link'),
+                'telegram_discuss_link' => $hasActivePlan
+                    ? config('v2board.telegram_discuss_link')
+                    : null,
                 'stripe_pk' => config('v2board.stripe_pk_live'),
                 'withdraw_methods' => config('v2board.commission_withdraw_method', Dict::WITHDRAW_METHOD_WHITELIST_DEFAULT),
                 'withdraw_close' => (int)config('v2board.withdraw_close_enable', 0),
